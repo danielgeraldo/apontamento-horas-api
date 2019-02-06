9ab2baadb4a4da3cfb740b2bd14b500d358a10d7
Daniel Geraldo <daniel.geraldo@gmail.com>
2019-02-06 15:29:40 -0200

    Arquivos iniciais do Projeto

---
+++ src/main/java/br/com/swb/apontamento/horas/apontamentoapi/ApontamentoApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.swb.apontamento.horas.apontamentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApontamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApontamentoApiApplication.class, args);
+	}
+
+}
+
